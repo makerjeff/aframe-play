a56d930062d3878b6a15d3ad6082abedf3e11779
Jefferson Wu <makerjeffwu@gmail.com>
2016-03-18 15:51:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+#Previewer Play
+RnD for Helena's previewer
+
+### Linksies!
+- [Uploading Files Locally](https://www.terlici.com/2015/05/16/uploading-files-locally.html)  **(DEPRECATED)**
+- [Express 4: File Upload](http://lollyrock.com/articles/express4-file-upload/)
+- [Using Multer requires middleware function error fix](http://stackoverflow.com/questions/31496100/cannot-app-usemulter-requires-middleware-function-error)
+- [File Upload using Node / Express / Multer](http://code.runnable.com/VNX-T8fdt5Y4X-mv/fileupload-for-node-js-and-hello-world) **(NEW!)** **(WORKS!)**
+- [MDN: Date.now() (Epoch time)](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now) **(NEW!)**
+- [Multer: NPM](https://github.com/expressjs/multer/issues/203) **(IMPORTANT!)**
+- [Vivus.JS](https://github.com/maxwellito/vivus) **(NEW!)**
+- [HTML5 Progress bar](http://www.matlus.com/html5-file-upload-with-progress/)
+- [HTML5 Rocks: XHR2](http://www.html5rocks.com/en/tutorials/file/xhr2/)
+
+### Reference
+- [Express JS - Middleware (book) (Has all the methods!)](https://ewiggin.gitbooks.io/expressjs-middleware/content/multer.html)
+- [Multer - Github](https://github.com/expressjs/multer)
+- [FormData-MDN](https://developer.mozilla.org/en-US/docs/Web/API/FormData/append)
+- [Request and Response Objects](http://www.murvinlai.com/req-and-res-in-nodejs.html)
+- [Parsing POST data in Express: Use BodyParser](http://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters-in-express)
+- [HTML Goodies: Drag and Drop files](http://www.htmlgoodies.com/html5/javascript/drag-files-into-the-browser-from-the-desktop-HTML5.html#fbid=uZVFQQwBxeJ)
+- [Mongoose Quick Start](http://mongoosejs.com/docs/)
+
+### Technologies
+- [adm-zip: backend zip file handling](https://github.com/cthackers/adm-zip)
+- [Multer: File upload]
+
+## Notes
+
+- File Upload using node/express/multer tutorial works.
+
+
+**Using Multer:<br>
+app.use(multer({dest:'./uploads/'}).single(...)); <br>
+app.use(multer({dest:'./uploads/'}).array(...)); <br>
+app.use(multer({dest:'./uploads/'}).fields(...)); <br>
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,110 @@
+/**
+ * 180 Previewer (transporter)
+ */
+
+/* MODULES */
+var express = require('express');
+var multer = require('multer');
+var colors = require('colors');
+var fileType = require('file-type');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+/*GLOBALS*/
+var done = false;
+
+
+/* INSTANCES */
+var app = express();
+var storage = multer.diskStorage({
+    destination: function(request, response, callback) {
+        callback(null, './uploads');
+    },
+    filename: function(request, file, callback){
+        callback(null, file.originalname);
+    }
+});
+
+//Multer upload module
+var upload = multer({storage: storage}).single('userPhoto');
+
+/* MIDDLEWARE */
+
+//server static files
+app.use(express.static(__dirname + "/public/"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//enable CORS
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// = logs to node console with every transaction
+//TODO Make this log to local file.
+app.use(function(request, response, next){
+    console.log('%s %s %s %s', request.method, request.url, request.path, colors.yellow(Date().toString()));
+    next();
+});
+
+/* ROUTES */
+
+//default route
+app.get('/', function(request, response){
+    response.sendFile(__dirname + '/public/index.html');
+});
+
+/
+// basic 404 catch-all middleware
+app.get('*', function(request, response){
+    response.sendFile(__dirname + '/public/404.html');
+});
+
+/* ===== START SERVER ===== */
+app.listen(3000, function(){
+    console.log('Working on Port 3000'.blue);
+});
+
+
+//TODO create folderArray of existing folders
+//TODO create random string folder, if folder exists in folderArray, try again.
+//TODO auto-unzip
+
+
+// ===== HELPER =====
+function logToFile(textString){
+
+    var logObject = {};
+
+    logObject.timestamp = Date.now() ;
+    logObject.content =  ': ' + '"' + textString + '"\n';
+
+    var contentString = Date.now() + ': ' + textString + '\n';
+
+    fs.appendFile('server-log.txt', logObject.timestamp + logObject.content, function(error){
+        if(error) {
+            return console.log(Error(error));
+        }
+    });
+}
+
+/**
+ * Make and ID.
+ * @param chars Number of characters to generate.
+ * @returns {string} Returns a random string of characters based on 'chars'.
+ */
+function createRandomFolder(chars){
+
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < chars; i++ ){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
